11207f9befaff9ddc614c1d99e279ba7fc879f1c
Sam Tomashi <samtomashikayembi@yahoo.fr>
2025-05-09 15:42:32 +0300

    added country property

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ class Car:
     allObjects = []
     speed = 0
 
+    country = "Kenya"
+
     def __init__(self, model, year, make):
         self.model = model
         self.year = year
@@ -15,11 +17,12 @@ class Car:
         self.calculating_speed()
 
     def breaking(self):
-        pass
+        self.speed
 
     def hooting():
         pass
 
+    @staticmethod
     def reversing():
         return "I am reversing"
   
@@ -30,17 +33,24 @@ class Car:
         return 180
     
 
-peugeot = Car("308", 2018, "Peugeot")
-prado = Car("TXL", 2020, "Prado")
-merdes = Car("X50", 2021, "Mercedes")
+peugeot = Car("208", 2020, "Peugeot")
+mobius = Car("Model 3", 2022, "Mobius")
 
-print(peugeot.make)
+peugeot.country = "France"
 
-print(prado.reversing())
+print(peugeot.country)
+
+print(mobius.country)
+
+print(mobius.year)
 
 
-for car in Car.allObjects:
-    print(car.make)
-
+'''
+Principles of OOP:
+1. Inheritance: Is the ability of a child class to inherit porperties and bhevaiouse of another class
+2. Encapsulation: The ability to protect of our code/classs/object from external modification
+3. Abstraction: The ability hide the complexity of our code/logic
+4. Polymorphism: The ability of a class to have many forms
+'''
 
 
